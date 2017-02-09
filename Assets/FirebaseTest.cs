1caa0425cadfdd23e7c30f89b317675a26bfebc6
Chatpong Suteesuksataporn <chatpong@progaming.co.th>
2017-02-09 17:50:48 +0700

    database and auth work

---
+++ Assets/FirebaseTest.cs
@@ -0,0 +1,183 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+using Firebase;
+using Firebase.Database;
+using Firebase.Unity.Editor;
+
+public class FirebaseTest : MonoBehaviour
+{
+    DatabaseReference mDatabase;
+    Firebase.Auth.FirebaseAuth auth;
+    Firebase.Auth.FirebaseUser user;
+    Firebase.DependencyStatus dependencyStatus = Firebase.DependencyStatus.UnavailableOther;
+
+    // Use this for initialization
+    void Start()
+    {
+        dependencyStatus = Firebase.FirebaseApp.CheckDependencies();
+
+        if (dependencyStatus != Firebase.DependencyStatus.Available)
+        {
+            Firebase.FirebaseApp.FixDependenciesAsync().ContinueWith(task =>
+            {
+                dependencyStatus = Firebase.FirebaseApp.CheckDependencies();
+                if (dependencyStatus == Firebase.DependencyStatus.Available)
+                {
+                    InitializeFirebase();
+                }
+                else
+                {
+                    Debug.LogError("Could not resolve all Firebase dependencies: " + dependencyStatus);
+                }
+            });
+        }
+        else
+        {
+            InitializeFirebase();
+        }
+    }
+
+    void InitializeFirebase()
+    {
+        // Set this before calling into the realtime database.
+        FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://progaming-playbento.firebaseio.com/");
+        
+        // Get the root reference location of the database.
+        mDatabase = FirebaseDatabase.DefaultInstance.RootReference;
+        
+        auth = Firebase.Auth.FirebaseAuth.DefaultInstance;
+        auth.StateChanged += AuthStateChanged;
+        AuthStateChanged(this, null);
+
+        // WriteNewUser("chatpongs", "Chatpong Suteesuksataporn", "chatpong@progaming.co.th");
+        // UpdateUser();
+        // WriteNewScore("chatpongs", 500);
+        // DeleteUser("chatpongs");
+
+        CreateUser("chatpong@progaming.co.th", "12345678");
+    }
+
+    private void WriteNewUser(string userId, string name, string email)
+    {
+        DatabaseReference users = mDatabase.Child("users");
+
+        User user = new User(name, email);
+        string json = JsonUtility.ToJson(user);
+
+        users.Child("chatpongs").SetRawJsonValueAsync(json);
+    }
+
+    private void UpdateUser()
+    {
+        mDatabase.Child("users").Child("chatpongs").Child("username").SetValueAsync("Nobita Nobi");
+    }
+
+    private void WriteNewScore(string userId, int score)
+    {
+        // Create new entry at /user-scores/$userid/$scoreid and at
+        // /leaderboard/$scoreid simultaneously
+        string key = mDatabase.Child("scores").Push().Key;
+        LeaderboardEntry entry = new LeaderboardEntry(userId, score);
+        Dictionary<string, object> entryValues = entry.ToDictionary();
+
+        Dictionary<string, object> childUpdates = new Dictionary<string, object>();
+        childUpdates["/scores/" + key] = entryValues;
+        childUpdates["/user-scores/" + userId + "/" + key] = entryValues;
+
+        mDatabase.UpdateChildrenAsync(childUpdates);
+    }
+
+    private void DeleteUser(string name)
+    {
+        mDatabase.Child("users").Child(name).RemoveValueAsync();
+    }
+
+    private void CreateUser(string email, string password)
+    {
+        auth.CreateUserWithEmailAndPasswordAsync(email, password).ContinueWith(task =>
+        {
+            if (task.IsCanceled)
+            {
+                Debug.LogError("CreateUserWithEmailAndPasswordAsync was canceled.");
+                return;
+            }
+            if (task.IsFaulted)
+            {
+                Debug.LogError("CreateUserWithEmailAndPasswordAsync encountered an error: " + task.Exception);
+                return;
+            }
+
+            // Firebase user has been created.
+            Firebase.Auth.FirebaseUser newUser = task.Result;
+            Debug.LogFormat("Firebase user created successfully: {0} ({1})",
+                newUser.DisplayName, newUser.UserId);
+        });
+    }
+
+    // Track state changes of the auth object.
+    private void AuthStateChanged(object sender, System.EventArgs eventArgs)
+    {
+        if (auth.CurrentUser != user)
+        {
+            bool signedIn = user != auth.CurrentUser && auth.CurrentUser != null;
+            if (!signedIn && user != null)
+            {
+                Debug.Log("Signed out " + user.UserId);
+            }
+            user = auth.CurrentUser;
+            if (signedIn)
+            {
+                Debug.Log("Signed in " + user.UserId);
+            }
+        }
+    }
+
+    private void OnDestroy()
+    {
+        auth.StateChanged -= AuthStateChanged;
+        auth = null;
+    }
+}
+
+public class User
+{
+    public string username;
+    public string email;
+
+    public User()
+    {
+    }
+
+    public User(string username, string email)
+    {
+        this.username = username;
+        this.email = email;
+    }
+}
+
+public class LeaderboardEntry
+{
+    public string uid;
+    public int score = 0;
+
+    public LeaderboardEntry()
+    {
+    }
+
+    public LeaderboardEntry(string uid, int score)
+    {
+        this.uid = uid;
+        this.score = score;
+    }
+
+    public Dictionary<string, object> ToDictionary()
+    {
+        Dictionary<string, object> result = new Dictionary<string, object>();
+        result["uid"] = uid;
+        result["score"] = score;
+
+        return result;
+    }
+}
